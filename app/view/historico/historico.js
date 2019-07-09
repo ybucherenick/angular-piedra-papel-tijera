5c0bfba39bc3a73443783f0be99be20f15a5b88c
ybucherenick <ybucherenick@gmail.com>
2019-07-09 18:58:54 -0400

    ventana historico

---
+++ app/view/historico/historico.js
@@ -0,0 +1,39 @@
+"use strict";
+
+(function () {
+    app.controller("HistoricoCtrl", [
+        "$scope",
+        "$q",
+        "$log",
+        "$PptClient",
+        function (
+            $scope,
+            $q,
+            $log,
+            $PptClient
+        ) {
+
+            loadData();
+            $scope.juego = {};
+            $scope.rounds = {};
+
+            function loadData() {
+                console.log("Entro al Historico")
+                $PptClient.getJuego().then(function (_result) {
+                    $scope.juego = _result.data.juego;
+                }, function (_error) {
+                    $log.error(_error);
+                })
+            }
+
+            $scope.verDetalle = function (_rounds) {
+                $scope.rounds = _rounds;
+                $('#modal').modal({ show: true });
+            }
+
+
+
+
+        }
+    ]);
+}.call());
